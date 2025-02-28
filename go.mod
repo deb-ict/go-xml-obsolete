da9587eed16280db0b34c11ab0461286b48838e8
Randy Deborggraeve <randy@deb-ict.com>
2025-02-28 21:00:02 +0100

    Add interfaces for etree serialization
    Signed-off-by: Randy Deborggraeve <randy@deb-ict.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/deb-ict/go-xml
 
 go 1.23.1
+
+require github.com/beevik/etree v1.5.0
---
+++ xml_node.go
@@ -0,0 +1,10 @@
+package xml
+
+import (
+	"github.com/beevik/etree"
+)
+
+type XmlNode interface {
+	LoadXml(resolver XmlResolver, el *etree.Element) error
+	GetXml(resolver XmlResolver) (*etree.Element, error)
+}
